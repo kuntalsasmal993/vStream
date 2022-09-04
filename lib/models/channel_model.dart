29f2f55644537bf13576593989971cc163cea8d4
Kuntal <kuntalsasmal993@gmail.com>
2022-09-04 14:39:58 +0530

    Vstream commit after API validaation

--- lib/models/channel_model.dart
+++ lib/models/channel_model.dart
@@ -1,46 +1,23 @@
-/// kind : "youtube#channelListResponse"
-/// etag : "6eV17xbODLQCVGCsfknDNRePoj0"
-/// pageInfo : {"totalResults":1,"resultsPerPage":5}
 /// items : [{"kind":"youtube#channel","etag":"BkF62uMhhTmHtD6Tw-_-Dsm8SqQ","id":"UC0zHs1JGEyqZKprMojNRQrg","snippet":{"title":"Kuntal Gaming","description":"Hello, welcome you in about section. Here you will get gameplays of different games. Don't forget to subscribe.","publishedAt":"2020-10-28T13:14:08.263624Z","thumbnails":{"default":{"url":"https://yt3.ggpht.com/PL6_dV8t_hDnxYb1O6hDbgdi5WswVOmVgdL-RHip4utxiILgvwFI5Q4FNJ_BB4M85L6nYFBR2Q=s88-c-k-c0x00ffffff-no-rj","width":88,"height":88},"medium":{"url":"https://yt3.ggpht.com/PL6_dV8t_hDnxYb1O6hDbgdi5WswVOmVgdL-RHip4utxiILgvwFI5Q4FNJ_BB4M85L6nYFBR2Q=s240-c-k-c0x00ffffff-no-rj","width":240,"height":240},"high":{"url":"https://yt3.ggpht.com/PL6_dV8t_hDnxYb1O6hDbgdi5WswVOmVgdL-RHip4utxiILgvwFI5Q4FNJ_BB4M85L6nYFBR2Q=s800-c-k-c0x00ffffff-no-rj","width":800,"height":800}},"localized":{"title":"Kuntal Gaming","description":"Hello, welcome you in about section. Here you will get gameplays of different games. Don't forget to subscribe."},"country":"IN"},"contentDetails":{"relatedPlaylists":{"likes":"","uploads":"UU0zHs1JGEyqZKprMojNRQrg"}},"statistics":{"viewCount":"471","subscriberCount":"47","hiddenSubscriberCount":false,"videoCount":"19"},"status":{"privacyStatus":"public","isLinked":true,"longUploadsStatus":"longUploadsUnspecified","madeForKids":false}}]
 
 class ChannelModel {
   ChannelModel({
-      this.kind, 
-      this.etag, 
-      this.pageInfo, 
-      this.items,});
+    this.items,});
 
   ChannelModel.fromJson(dynamic json) {
-    kind = json['kind'];
-    etag = json['etag'];
-    pageInfo = json['pageInfo'] != null ? PageInfo.fromJson(json['pageInfo']) : null;
-    if (json['items'] != null) {
+    if (json[0]['items'] != null) {
       items = [];
-      json['items'].forEach((v) {
+      json[0]['items'].forEach((v) {
         items?.add(Items.fromJson(v));
       });
     }
   }
-  String? kind;
-  String? etag;
-  PageInfo? pageInfo;
   List<Items>? items;
-ChannelModel copyWith({  String? kind,
-  String? etag,
-  PageInfo? pageInfo,
-  List<Items>? items,
-}) => ChannelModel(  kind: kind ?? this.kind,
-  etag: etag ?? this.etag,
-  pageInfo: pageInfo ?? this.pageInfo,
-  items: items ?? this.items,
-);
+  ChannelModel copyWith({  List<Items>? items,
+  }) => ChannelModel(  items: items ?? this.items,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
-    map['kind'] = kind;
-    map['etag'] = etag;
-    if (pageInfo != null) {
-      map['pageInfo'] = pageInfo?.toJson();
-    }
     if (items != null) {
       map['items'] = items?.map((v) => v.toJson()).toList();
     }
@@ -59,13 +36,13 @@ ChannelModel copyWith({  String? kind,
 
 class Items {
   Items({
-      this.kind, 
-      this.etag, 
-      this.id, 
-      this.snippet, 
-      this.contentDetails, 
-      this.statistics, 
-      this.status,});
+    this.kind,
+    this.etag,
+    this.id,
+    this.snippet,
+    this.contentDetails,
+    this.statistics,
+    this.status,});
 
   Items.fromJson(dynamic json) {
     kind = json['kind'];
@@ -83,21 +60,21 @@ class Items {
   ContentDetails? contentDetails;
   Statistics? statistics;
   Status? status;
-Items copyWith({  String? kind,
-  String? etag,
-  String? id,
-  Snippet? snippet,
-  ContentDetails? contentDetails,
-  Statistics? statistics,
-  Status? status,
-}) => Items(  kind: kind ?? this.kind,
-  etag: etag ?? this.etag,
-  id: id ?? this.id,
-  snippet: snippet ?? this.snippet,
-  contentDetails: contentDetails ?? this.contentDetails,
-  statistics: statistics ?? this.statistics,
-  status: status ?? this.status,
-);
+  Items copyWith({  String? kind,
+    String? etag,
+    String? id,
+    Snippet? snippet,
+    ContentDetails? contentDetails,
+    Statistics? statistics,
+    Status? status,
+  }) => Items(  kind: kind ?? this.kind,
+    etag: etag ?? this.etag,
+    id: id ?? this.id,
+    snippet: snippet ?? this.snippet,
+    contentDetails: contentDetails ?? this.contentDetails,
+    statistics: statistics ?? this.statistics,
+    status: status ?? this.status,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['kind'] = kind;
@@ -127,10 +104,10 @@ Items copyWith({  String? kind,
 
 class Status {
   Status({
-      this.privacyStatus, 
-      this.isLinked, 
-      this.longUploadsStatus, 
-      this.madeForKids,});
+    this.privacyStatus,
+    this.isLinked,
+    this.longUploadsStatus,
+    this.madeForKids,});
 
   Status.fromJson(dynamic json) {
     privacyStatus = json['privacyStatus'];
@@ -142,15 +119,15 @@ class Status {
   bool? isLinked;
   String? longUploadsStatus;
   bool? madeForKids;
-Status copyWith({  String? privacyStatus,
-  bool? isLinked,
-  String? longUploadsStatus,
-  bool? madeForKids,
-}) => Status(  privacyStatus: privacyStatus ?? this.privacyStatus,
-  isLinked: isLinked ?? this.isLinked,
-  longUploadsStatus: longUploadsStatus ?? this.longUploadsStatus,
-  madeForKids: madeForKids ?? this.madeForKids,
-);
+  Status copyWith({  String? privacyStatus,
+    bool? isLinked,
+    String? longUploadsStatus,
+    bool? madeForKids,
+  }) => Status(  privacyStatus: privacyStatus ?? this.privacyStatus,
+    isLinked: isLinked ?? this.isLinked,
+    longUploadsStatus: longUploadsStatus ?? this.longUploadsStatus,
+    madeForKids: madeForKids ?? this.madeForKids,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['privacyStatus'] = privacyStatus;
@@ -169,10 +146,10 @@ Status copyWith({  String? privacyStatus,
 
 class Statistics {
   Statistics({
-      this.viewCount, 
-      this.subscriberCount, 
-      this.hiddenSubscriberCount, 
-      this.videoCount,});
+    this.viewCount,
+    this.subscriberCount,
+    this.hiddenSubscriberCount,
+    this.videoCount,});
 
   Statistics.fromJson(dynamic json) {
     viewCount = json['viewCount'];
@@ -184,15 +161,15 @@ class Statistics {
   String? subscriberCount;
   bool? hiddenSubscriberCount;
   String? videoCount;
-Statistics copyWith({  String? viewCount,
-  String? subscriberCount,
-  bool? hiddenSubscriberCount,
-  String? videoCount,
-}) => Statistics(  viewCount: viewCount ?? this.viewCount,
-  subscriberCount: subscriberCount ?? this.subscriberCount,
-  hiddenSubscriberCount: hiddenSubscriberCount ?? this.hiddenSubscriberCount,
-  videoCount: videoCount ?? this.videoCount,
-);
+  Statistics copyWith({  String? viewCount,
+    String? subscriberCount,
+    bool? hiddenSubscriberCount,
+    String? videoCount,
+  }) => Statistics(  viewCount: viewCount ?? this.viewCount,
+    subscriberCount: subscriberCount ?? this.subscriberCount,
+    hiddenSubscriberCount: hiddenSubscriberCount ?? this.hiddenSubscriberCount,
+    videoCount: videoCount ?? this.videoCount,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['viewCount'] = viewCount;
@@ -208,15 +185,15 @@ Statistics copyWith({  String? viewCount,
 
 class ContentDetails {
   ContentDetails({
-      this.relatedPlaylists,});
+    this.relatedPlaylists,});
 
   ContentDetails.fromJson(dynamic json) {
     relatedPlaylists = json['relatedPlaylists'];
   }
   RelatedPlaylists? relatedPlaylists;
-ContentDetails copyWith({  RelatedPlaylists? relatedPlaylists,
-}) => ContentDetails(  relatedPlaylists: relatedPlaylists ?? this.relatedPlaylists,
-);
+  ContentDetails copyWith({  RelatedPlaylists? relatedPlaylists,
+  }) => ContentDetails(  relatedPlaylists: relatedPlaylists ?? this.relatedPlaylists,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['relatedPlaylists'] = relatedPlaylists;
@@ -230,8 +207,8 @@ ContentDetails copyWith({  RelatedPlaylists? relatedPlaylists,
 
 class RelatedPlaylists {
   RelatedPlaylists({
-      this.likes, 
-      this.uploads,});
+    this.likes,
+    this.uploads,});
 
   RelatedPlaylists.fromJson(dynamic json) {
     likes = json['likes'];
@@ -239,11 +216,11 @@ class RelatedPlaylists {
   }
   String? likes;
   String? uploads;
-RelatedPlaylists copyWith({  String? likes,
-  String? uploads,
-}) => RelatedPlaylists(  likes: likes ?? this.likes,
-  uploads: uploads ?? this.uploads,
-);
+  RelatedPlaylists copyWith({  String? likes,
+    String? uploads,
+  }) => RelatedPlaylists(  likes: likes ?? this.likes,
+    uploads: uploads ?? this.uploads,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['likes'] = likes;
@@ -262,12 +239,12 @@ RelatedPlaylists copyWith({  String? likes,
 
 class Snippet {
   Snippet({
-      this.title, 
-      this.description, 
-      this.publishedAt, 
-      this.thumbnails, 
-      this.localized, 
-      this.country,});
+    this.title,
+    this.description,
+    this.publishedAt,
+    this.thumbnails,
+    this.localized,
+    this.country,});
 
   Snippet.fromJson(dynamic json) {
     title = json['title'];
@@ -283,19 +260,19 @@ class Snippet {
   Thumbnails? thumbnails;
   Localized? localized;
   String? country;
-Snippet copyWith({  String? title,
-  String? description,
-  String? publishedAt,
-  Thumbnails? thumbnails,
-  Localized? localized,
-  String? country,
-}) => Snippet(  title: title ?? this.title,
-  description: description ?? this.description,
-  publishedAt: publishedAt ?? this.publishedAt,
-  thumbnails: thumbnails ?? this.thumbnails,
-  localized: localized ?? this.localized,
-  country: country ?? this.country,
-);
+  Snippet copyWith({  String? title,
+    String? description,
+    String? publishedAt,
+    Thumbnails? thumbnails,
+    Localized? localized,
+    String? country,
+  }) => Snippet(  title: title ?? this.title,
+    description: description ?? this.description,
+    publishedAt: publishedAt ?? this.publishedAt,
+    thumbnails: thumbnails ?? this.thumbnails,
+    localized: localized ?? this.localized,
+    country: country ?? this.country,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['title'] = title;
@@ -318,8 +295,8 @@ Snippet copyWith({  String? title,
 
 class Localized {
   Localized({
-      this.title, 
-      this.description,});
+    this.title,
+    this.description,});
 
   Localized.fromJson(dynamic json) {
     title = json['title'];
@@ -327,11 +304,11 @@ class Localized {
   }
   String? title;
   String? description;
-Localized copyWith({  String? title,
-  String? description,
-}) => Localized(  title: title ?? this.title,
-  description: description ?? this.description,
-);
+  Localized copyWith({  String? title,
+    String? description,
+  }) => Localized(  title: title ?? this.title,
+    description: description ?? this.description,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['title'] = title;
@@ -347,9 +324,9 @@ Localized copyWith({  String? title,
 
 class Thumbnails {
   Thumbnails({
-      this.defaultThumb,
-      this.medium, 
-      this.high,});
+    this.defaultThumb,
+    this.medium,
+    this.high,});
 
   Thumbnails.fromJson(dynamic json) {
     defaultThumb = json['default'] != null ? DefaultThumb.fromJson(json['default']) : null;
@@ -388,9 +365,9 @@ class Thumbnails {
 
 class High {
   High({
-      this.url, 
-      this.width, 
-      this.height,});
+    this.url,
+    this.width,
+    this.height,});
 
   High.fromJson(dynamic json) {
     url = json['url'];
@@ -400,13 +377,13 @@ class High {
   String? url;
   int? width;
   int? height;
-High copyWith({  String? url,
-  int? width,
-  int? height,
-}) => High(  url: url ?? this.url,
-  width: width ?? this.width,
-  height: height ?? this.height,
-);
+  High copyWith({  String? url,
+    int? width,
+    int? height,
+  }) => High(  url: url ?? this.url,
+    width: width ?? this.width,
+    height: height ?? this.height,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['url'] = url;
@@ -423,9 +400,9 @@ High copyWith({  String? url,
 
 class Medium {
   Medium({
-      this.url, 
-      this.width, 
-      this.height,});
+    this.url,
+    this.width,
+    this.height,});
 
   Medium.fromJson(dynamic json) {
     url = json['url'];
@@ -435,13 +412,13 @@ class Medium {
   String? url;
   int? width;
   int? height;
-Medium copyWith({  String? url,
-  int? width,
-  int? height,
-}) => Medium(  url: url ?? this.url,
-  width: width ?? this.width,
-  height: height ?? this.height,
-);
+  Medium copyWith({  String? url,
+    int? width,
+    int? height,
+  }) => Medium(  url: url ?? this.url,
+    width: width ?? this.width,
+    height: height ?? this.height,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['url'] = url;
@@ -456,11 +433,11 @@ Medium copyWith({  String? url,
 /// width : 88
 /// height : 88
 
-class DefaultThumb {
+class DefaultThumb{
   DefaultThumb({
-      this.url, 
-      this.width, 
-      this.height,});
+    this.url,
+    this.width,
+    this.height,});
 
   DefaultThumb.fromJson(dynamic json) {
     url = json['url'];
@@ -471,45 +448,17 @@ class DefaultThumb {
   int? width;
   int? height;
   DefaultThumb copyWith({  String? url,
-  int? width,
-  int? height,
-}) => DefaultThumb(  url: url ?? this.url,
-  width: width ?? this.width,
-  height: height ?? this.height,
-);
+    int? width,
+    int? height,
+  }) => DefaultThumb(  url: url ?? this.url,
+    width: width ?? this.width,
+    height: height ?? this.height,
+  );
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['url'] = url;
     map['width'] = width;
     map['height'] = height;
-    return map;
-  }
-
-}
-
-/// totalResults : 1
-/// resultsPerPage : 5
-
-class PageInfo {
-  PageInfo({
-      this.totalResults, 
-      this.resultsPerPage,});
-
-  PageInfo.fromJson(dynamic json) {
-    totalResults = json['totalResults'];
-    resultsPerPage = json['resultsPerPage'];
-  }
-  int? totalResults;
-  int? resultsPerPage;
-PageInfo copyWith({  int? totalResults,
-  int? resultsPerPage,
-}) => PageInfo(  totalResults: totalResults ?? this.totalResults,
-  resultsPerPage: resultsPerPage ?? this.resultsPerPage,
-);
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['totalResults'] = totalResults;
-    map['resultsPerPage'] = resultsPerPage;
     return map;
   }
 
